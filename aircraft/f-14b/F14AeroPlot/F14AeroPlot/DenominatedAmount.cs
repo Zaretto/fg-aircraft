ee75d6f12c253ddd34afebc2324c6784c7d11036
Richard Harrison <rjh@zaretto.com>
2016-05-27 19:33:57 +0200

    AeroTool: Changes to make compatible with my new standard aircraft model template.

--- aircraft/f-14b/F14AeroPlot/F14AeroPlot/DenominatedAmount.cs
+++ aircraft/f-14b/F14AeroPlot/F14AeroPlot/DenominatedAmount.cs
@@ -1,20 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Xml;
 
 namespace F14AeroPlot
 {
     public class DenominatedAmount
     {
-        public DenominatedAmount(double v, string u)
+        public DenominatedAmount(double v, string u, int? decimal_places = null)
         {
+            DecimalPlaces = decimal_places;
             Amount = v;
             Unit = u;
         }
 
         public double Amount { get; private set; }
+
+        public int? DecimalPlaces { get; set; }
 
         public string Unit { get; private set; }
 
@@ -23,8 +23,19 @@ namespace F14AeroPlot
             var node = doc.CreateElement(id);
             if (Unit != null)
                 node.SetAttribute("unit", Unit);
-            node.InnerText = Amount.ToString();
+            if (DecimalPlaces.HasValue)
+            {
+                var format = "{0:F" + DecimalPlaces.ToString() + "}";
+                node.InnerText = String.Format(format, Amount);
+            }
+            else
+                node.InnerText = Amount.ToString();
             return node;
+        }
+
+        internal void SetAmount(double newval)
+        {
+            Amount = newval;
         }
     }
 }
